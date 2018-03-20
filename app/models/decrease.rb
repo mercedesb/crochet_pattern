150731ba3d0759e1240afee81aca8782881def6d
mercedesb <mercedesbernard@live.com>
2018-03-19 19:44:56 -0500

    added decrease model

---
+++ app/models/decrease.rb
@@ -0,0 +1,21 @@
+# Decorator pattern
+class Decrease < BaseStitch
+  attr_reader :children
+
+  def initialize(children = [])
+    @children = children
+  end
+
+  def make
+    "#{@children.map(&:make).join(' ')} #{self.class.abbrev}"
+  end
+
+  def self.abbrev
+    'dec'
+  end
+
+  def count
+    children_count = @children.sum(&:count)
+    children_count - 1
+  end
+end
---
+++ spec/models/decrease_spec.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Decrease, type: :model do
+  subject { described_class.new([SingleCrochet.new]) }
+  describe '#make' do
+    it 'returns dec' do
+      expect(subject.make).to eq 'sc dec'
+    end
+  end
+
+  describe '#abbrev' do
+    it 'returns dec' do
+      expect(Decrease.abbrev).to eq 'dec'
+    end
+  end
+
+  describe '#count' do
+    it 'returns -1' do
+      expect(subject.count).to eq 0
+    end
+  end
+end
